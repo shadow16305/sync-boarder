2d53185e37e0dbed2b5595f89d7c19ed6aa8fe6f
shadow16305 <chris16305@gmail.com>
2024-10-12 15:44:07 +0200

    Add: Landing page

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="h-screen flex flex-col items-center justify-center space-y-4 text-center absolute inset-0 overflow-hidden">
+      <h1 className="text-5xl font-bold max-w-2xl leading-tight">
+        SyncBoarder helps teams{" "}
+        <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md px-4 text-white">
+          move work forward
+        </span>
+      </h1>
+      <p className="text-xl max-w-2xl text-neutral-500">
+        Streamline your tasks and discussions seamlessly with our intuitive task board and integrated chat features.
+        Stay synchronized, stay productive. Get started today!
+      </p>
+      <Button asChild size="lg">
+        <Link href="/sign-in">Get Started</Link>
+      </Button>
+    </main>
+  );
+}
